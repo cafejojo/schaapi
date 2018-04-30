e7b8c500b572e49836dcb7bbc30b6a792f8b3aa9
Georgios Andreadis <g.andreadis@student.tudelft.nl>
2018-04-30 17:31:56 +0200

    Apply enumerator to new datastructure

--- subprojects/pattern-detector/src/main/kotlin/org/cafejojo/schaapi/patterndetector/PathEnumerator.kt
+++ subprojects/pattern-detector/src/main/kotlin/org/cafejojo/schaapi/patterndetector/PathEnumerator.kt
@@ -1,24 +1,33 @@
 package org.cafejojo.schaapi.patterndetector
 
+import org.cafejojo.schaapi.usagegraphgenerator.BranchNode
+import org.cafejojo.schaapi.usagegraphgenerator.EntryNode
+import org.cafejojo.schaapi.usagegraphgenerator.ExitNode
+import org.cafejojo.schaapi.usagegraphgenerator.Node
 import java.util.Stack
 
 internal data class SearchState(val node: Node, val nextNeighbourIndex: Int)
 
-fun enumeratePaths(source: Node, sink: Node): Set<List<PathItem>> {
-    val paths = mutableSetOf<List<PathItem>>()
+internal fun Node.toSingleSuccessor(successorIndex: Int, exitNode: ExitNode): Node = when (this) {
+    is BranchNode -> this.getSingleSuccessorCopy(successorIndex, exitNode)
+    else -> this
+}
+
+fun enumeratePaths(entryNode: EntryNode, exitNode: ExitNode): Set<List<Node>> {
+    val paths = mutableSetOf<List<Node>>()
     val searchStack = Stack<SearchState>()
     val visited = mutableSetOf<Node>()
-    val currentPath = mutableListOf<PathItem>()
+    val currentPath = mutableListOf<Node>()
 
-    searchStack.push(SearchState(source, 0))
-    visited.add(source)
-    currentPath.add(nodeToPathItem(source))
+    searchStack.push(SearchState(entryNode, 0))
+    visited.add(entryNode)
+    currentPath.add(entryNode.toSingleSuccessor(0, exitNode))
 
     while (!searchStack.empty()) {
         val state = searchStack.peek()
 
-        if (state.node == sink || state.nextNeighbourIndex == state.node.outgoingEdges.size) {
-            if (state.node == sink) {
+        if (state.node == exitNode || state.nextNeighbourIndex == state.node.successors.size) {
+            if (state.node == exitNode) {
                 paths.add(currentPath)
             }
 
@@ -27,7 +36,7 @@ fun enumeratePaths(source: Node, sink: Node): Set<List<PathItem>> {
             currentPath.removeAt(currentPath.size - 1)
             searchStack.pop()
         } else {
-            val nextNode = state.node.outgoingEdges.elementAt(state.nextNeighbourIndex).to
+            val nextNode = state.node.successors[state.nextNeighbourIndex]
 
             // Move on to next neighbour
             searchStack.pop()
@@ -36,7 +45,7 @@ fun enumeratePaths(source: Node, sink: Node): Set<List<PathItem>> {
             if (!visited.contains(nextNode)) {
                 searchStack.push(SearchState(nextNode, 0))
                 visited.add(nextNode)
-                currentPath.add(nodeToPathItem(nextNode))
+                currentPath.add(nextNode.toSingleSuccessor(0, exitNode))
             }
         }
     }
