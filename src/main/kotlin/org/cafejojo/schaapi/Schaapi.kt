057748d53d92ee5f3eb661163f44aeede7b9e00f
Felix Dekker <felix@fwdekker.com>
2018-04-30 14:05:46 +0200

    Set up testing libraries

--- src/main/kotlin/org/cafejojo/schaapi/Schaapi.kt
+++ src/main/kotlin/org/cafejojo/schaapi/Schaapi.kt
@@ -4,3 +4,10 @@ package org.cafejojo.schaapi
 fun main(args: Array<String>) {
     println("I am the main main class!")
 }
+
+
+class Schaapi {
+    fun testMe(foo: Int): Int {
+        return 2 * foo
+    }
+}
--- subprojects/pattern-detector/src/main/kotlin/org/cafejojo/schaapi/patterndetector/PatternDetector.kt
+++ subprojects/pattern-detector/src/main/kotlin/org/cafejojo/schaapi/patterndetector/PatternDetector.kt
@@ -4,3 +4,10 @@ package org.cafejojo.schaapi.patterndetector
 fun main(args: Array<String>) {
     println("I am the pattern detector!")
 }
+
+
+class PatternDetector {
+    fun testMe(foo: String): String {
+        return foo + foo
+    }
+}
