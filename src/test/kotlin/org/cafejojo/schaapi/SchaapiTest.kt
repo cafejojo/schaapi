965172ffaead297c1ec8a96c259729a19f07c92a
Felix Dekker <felix@fwdekker.com>
2018-04-30 17:09:04 +0200

    Fix failing test in root module

--- src/test/kotlin/org/cafejojo/schaapi/SchaapiTest.kt
+++ src/test/kotlin/org/cafejojo/schaapi/SchaapiTest.kt
@@ -9,7 +9,7 @@ internal class SchaapiTest : Spek({
     describe("Schaapi") {
         it("should work for negative numbers") {
             assertThat(Schaapi().testMe(-49643))
-                .isEqualTo(-99288)
+                .isEqualTo(-99286)
         }
 
         it("should work for 0") {
