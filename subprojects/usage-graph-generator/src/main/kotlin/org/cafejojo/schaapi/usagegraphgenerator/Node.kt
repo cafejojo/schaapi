1c2ff56e3a9c3ce9642c57d4e763f96f6f109fd5
Casper Boone <mail@casperboone.nl>
2018-05-01 12:17:40 +0200

    Update KDoc to include properties

--- subprojects/usage-graph-generator/src/main/kotlin/org/cafejojo/schaapi/usagegraphgenerator/Node.kt
+++ subprojects/usage-graph-generator/src/main/kotlin/org/cafejojo/schaapi/usagegraphgenerator/Node.kt
@@ -7,6 +7,8 @@ import java.util.UUID
  * Represents a statement node.
  *
  * Contains references to the successor nodes.
+ *
+ * @property id a unique id for the node.
  */
 abstract class Node(val successors: MutableList<Node> = arrayListOf(), val id: NodeId = UuidNodeId()) {
     override fun equals(other: Any?) = when {
