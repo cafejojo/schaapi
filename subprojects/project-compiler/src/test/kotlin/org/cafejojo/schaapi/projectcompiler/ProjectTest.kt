26b93381b4665e3aba82ea9be04dd9761a97f73c
Felix Dekker <felix@fwdekker.com>
2018-05-06 15:44:01 +0200

    Extract test case ZIPs

--- subprojects/project-compiler/src/test/kotlin/org/cafejojo/schaapi/projectcompiler/ProjectTest.kt
+++ subprojects/project-compiler/src/test/kotlin/org/cafejojo/schaapi/projectcompiler/ProjectTest.kt
@@ -1,7 +1,7 @@
 package org.cafejojo.schaapi.projectcompiler
 
-import net.lingala.zip4j.core.ZipFile
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.fail
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
@@ -11,32 +11,33 @@ internal class ProjectTest : Spek({
     describe("projects") {
         val target = File("./test")
 
+        beforeEachTest {
+            val projectURI = javaClass.getResource("/Project/dependencies-classes")
+            if (projectURI == null) {
+                fail("Project source directory could not be found.")
+            }
+
+            val projectFiles = File(projectURI.toURI())
+            projectFiles.copyRecursively(target, true)
+        }
+
         afterEachTest {
             target.deleteRecursively()
         }
 
         it("knows which classes it contains") {
-            val projectZip = javaClass.getResource("/Project/dependencies-classes.zip")
-            ZipFile(projectZip.path).extractAll(target.absolutePath)
-
             val project = Project(target)
 
-            assertThat(project.containsClass("org.cafejojo.schaapi.test.MyClass")).isTrue()
+            assertThat(project.containsClass("MyClass")).isFalse()
         }
 
         it("is specific about packages") {
-            val projectZip = javaClass.getResource("/Project/dependencies-classes.zip")
-            ZipFile(projectZip.path).extractAll(target.absolutePath)
-
             val project = Project(target)
 
             assertThat(project.containsClass("MyClass")).isFalse()
         }
 
         it("does not think it contains classes from its dependencies") {
-            val projectZip = javaClass.getResource("/Project/dependencies-classes.zip")
-            ZipFile(projectZip.path).extractAll(target.absolutePath)
-
             val project = Project(target)
 
             assertThat(project.containsClass("net.lingala.zip4j.core.ZipFile")).isFalse()
