5e8a15a79f4075732c7a04861fe13c200181037f
Joel Abrahams <abrahamsjo@gmail.com>
2018-05-07 16:20:46 +0200

    Add generating of method params

---
+++ subprojects/test-generator/src/test/kotlin/org/cafejojo/schaapi/testgenerator/TestGeneratorTest.kt
@@ -0,0 +1,10 @@
+package org.cafejojo.schaapi.testgenerator
+
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+
+internal class TestGeneratorTest : Spek({
+    describe("") {
+
+    }
+})
