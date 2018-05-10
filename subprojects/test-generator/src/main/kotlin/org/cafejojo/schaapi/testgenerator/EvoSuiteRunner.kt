17008dc67f2a696157ac97c567ef9106cb3bcc16
Georgios Andreadis <g.andreadis@student.tudelft.nl>
2018-05-10 12:57:19 +0200

    Launch EvoSuite on new process

--- subprojects/test-generator/src/main/kotlin/org/cafejojo/schaapi/testgenerator/EvoSuiteRunner.kt
+++ subprojects/test-generator/src/main/kotlin/org/cafejojo/schaapi/testgenerator/EvoSuiteRunner.kt
@@ -1,28 +1,72 @@
 package org.cafejojo.schaapi.testgenerator
 
-import org.evosuite.EvoSuite
+import java.io.BufferedReader
+import java.io.File
+import java.io.InputStreamReader
+import java.net.URLClassLoader
 
-object EvoSuiteRunner {
-    fun run(fullyQualifiedClassName: String, classPath: String, outputDirectory: String, generationTimeout: Int = 60) {
-        val evosuite = EvoSuite()
-        evosuite.parseCommandLine(
-            arrayOf(
+
+/**
+ * EvoSuite launcher.
+ *
+ * Executes the EvoSuite test generator on a new process and returns once that process finishes.
+ *
+ * @property fullyQualifiedClassName the class that EvoSuite should generate tests for
+ * @property classPath the class path on which to find the class that should be tested
+ * @property outputDirectory the output directory path for the generated EvoSuite tests
+ * @property generationTimeoutSeconds how long to let the EvoSuite test generator run (in seconds)
+ * @property logEvoSuiteOutput whether to output EvoSuite logs to [System.out]
+ */
+class EvoSuiteRunner(
+        private val fullyQualifiedClassName: String,
+        private val classPath: String,
+        private val outputDirectory: String,
+        private val generationTimeoutSeconds: Int = 60,
+        private val logEvoSuiteOutput: Boolean = false
+) {
+    private val currentClassPath: String
+
+    init {
+        val classLoader = ClassLoader.getSystemClassLoader()
+        val urls = (classLoader as URLClassLoader).urLs
+        currentClassPath = urls.joinToString(separator = File.pathSeparator) { it.file }
+    }
+
+    /**
+     * Runs the EvoSuite test generator in a new process.
+     */
+    fun run() {
+        val process = buildProcess()
+        receiveProcessOutput(process)
+    }
+
+    private fun buildProcess(): Process {
+        val processBuilder = ProcessBuilder(
+                "java", "-cp", currentClassPath, "org.evosuite.EvoSuite",
                 "-class", fullyQualifiedClassName,
                 "-base_dir", outputDirectory,
                 "-projectCP", classPath,
-                "-Dsearch_budget=$generationTimeout",
+                "-Dsearch_budget=$generationTimeoutSeconds",
                 "-Dstopping_condition=MaxTime"
-            )
         )
 
-//        val handler = ExternalProcessHandler()
-//        handler.closeServer()
+        return processBuilder.start()
+    }
 
-//        println(MasterServices.getInstance()
-        println("yeeyeyeye")
-//        println(Thread.getAllStackTraces().keys.size)
-//        Thread.getAllStackTraces().keys.forEach {
-//            it.join()
-//        }
+    private fun receiveProcessOutput(process: Process) {
+        val bufferedReader = BufferedReader(InputStreamReader(process.inputStream))
+        var line = bufferedReader.readLine()
+        while (line != null) {
+            if (logEvoSuiteOutput) println(line)
+
+            line = bufferedReader.readLine()
+        }
+
+        if (process.exitValue() != 0) {
+            throw EvoSuiteRuntimeException("EvoSuite exited with non-zero exit code: " +
+                    "${process.exitValue()} - ${process.errorStream}")
+        }
     }
 }
+
+class EvoSuiteRuntimeException(message: String? = null) : RuntimeException(message)
--- subprojects/test-generator/src/test/kotlin/org/cafejojo/schaapi/testgenerator/EvoSuiteRunnerTest.kt
+++ subprojects/test-generator/src/test/kotlin/org/cafejojo/schaapi/testgenerator/EvoSuiteRunnerTest.kt
@@ -1,51 +1,41 @@
 package org.cafejojo.schaapi.testgenerator
 
+import org.assertj.core.api.Assertions.assertThat
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.io.PrintStream
+import java.io.File
 
 internal class EvoSuiteRunnerTest : Spek({
     val classPath = EvoSuiteRunnerTest::class.java.getResource("../../../../").toURI().path
+    val evoSuiteTestOutput = File("$classPath/evosuite-tests/")
+
+    fun deleteTestOutput() {
+        if (evoSuiteTestOutput.exists()) {
+            evoSuiteTestOutput.deleteRecursively()
+        }
+    }
+
+    beforeGroup {
+        deleteTestOutput()
+    }
+
+    afterEachTest {
+        deleteTestOutput()
+
+    }
 
     describe("execution of the EvoSuite test generator") {
         it("generates tests for a test class") {
-            EvoSuiteRunner.run(
+            EvoSuiteRunner(
                 "org.cafejojo.schaapi.test.EvoSuiteTestClass",
                 classPath,
                 classPath,
-                generationTimeout = 5
-            )
-//            assertThat(File("$classPath/evosuite-tests/org/cafejojo/schaapi/test/EvoSuiteTestClass_ESTest.java")).exists()
+                    generationTimeoutSeconds = 5
+            ).run()
+
+            assertThat(File("${evoSuiteTestOutput.path}/org/cafejojo/schaapi/test/EvoSuiteTestClass_ESTest.java"))
+                    .exists()
         }
     }
 })
-
-fun main(args: Array<String>) {
-    val classPath = EvoSuiteRunnerTest::class.java.getResource("../../../../").toURI().path
-
-    val process = Runtime.getRuntime().exec("java -cp $classPath org.evosuite.EvoSuite")
-    process.waitFor()
-    val error = BufferedReader(InputStreamReader(process.errorStream))
-    error.lines().forEach {
-        System.out.println(it)
-    }
-    error.close()
-    val outputStream = process.outputStream
-    val printStream = PrintStream(outputStream)
-    printStream.println()
-    printStream.flush()
-    printStream.close()
-//    val evosuite = EvoSuite()
-//    evosuite.parseCommandLine(
-//        arrayOf(
-//            "-class", "org.cafejojo.schaapi.test.EvoSuiteTestClass",
-//            "-base_dir", classPath,
-//            "-projectCP", classPath,
-//            "-Dsearch_budget=5",
-//            "-Dstopping_condition=MaxTime"
-//        )
-//    )
-}
