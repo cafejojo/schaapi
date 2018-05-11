e44240b3dfd630e9db573a90d13b2d471aab81fa
Georgios Andreadis <g.andreadis@student.tudelft.nl>
2018-05-11 09:47:55 +0200

    Add check for last line printed by EvoSuite

--- subprojects/test-generator/src/main/kotlin/org/cafejojo/schaapi/testgenerator/EvoSuiteRunner.kt
+++ subprojects/test-generator/src/main/kotlin/org/cafejojo/schaapi/testgenerator/EvoSuiteRunner.kt
@@ -2,7 +2,7 @@ package org.cafejojo.schaapi.testgenerator
 
 import java.io.BufferedReader
 import java.io.InputStreamReader
-import java.io.OutputStream
+import java.io.PrintStream
 import java.nio.charset.Charset
 
 /**
@@ -14,14 +14,14 @@ import java.nio.charset.Charset
  * @property classPath the class path on which to find the class that should be tested
  * @property outputDirectory the output directory path for the generated EvoSuite tests
  * @property generationTimeoutSeconds how long to let the EvoSuite test generator run (in seconds)
- * @property logEvoSuiteOutput whether to output EvoSuite logs to [System.out]
+ * @property evoSuitePrintStream whether to output EvoSuite logs to [System.out]
  */
 class EvoSuiteRunner(
         private val fullyQualifiedClassName: String,
         private val classPath: String,
         private val outputDirectory: String,
         private val generationTimeoutSeconds: Int = 60,
-        private val evoSuiteOutputStream: OutputStream? = null
+        private val evoSuitePrintStream: PrintStream? = null
 ) {
     /**
      * Runs the EvoSuite test generator in a new process.
@@ -41,14 +41,19 @@ class EvoSuiteRunner(
 
     private fun receiveProcessOutput(process: Process) {
         val bufferedReader = BufferedReader(InputStreamReader(process.inputStream))
-        var line = bufferedReader.readLine()
-        while (line != null) {
-            evoSuiteOutputStream?.write(line.toByteArray())
+        var lastLine = ""
 
-            line = bufferedReader.readLine()
+        bufferedReader.forEachLine {
+            evoSuitePrintStream?.println(it)
+            lastLine = it
         }
 
         process.waitFor()
+
+        if (!lastLine.toLowerCase().contains("computation finished")) {
+            throw EvoSuiteRuntimeException("EvoSuite did not terminate successfully.")
+        }
+
         if (process.exitValue() != 0) {
             val errorOutput = String(process.errorStream.readBytes(), Charset.defaultCharset())
             throw EvoSuiteRuntimeException("EvoSuite exited with non-zero exit code: " +
--- subprojects/test-generator/src/test/kotlin/org/cafejojo/schaapi/testgenerator/EvoSuiteRunnerTest.kt
+++ subprojects/test-generator/src/test/kotlin/org/cafejojo/schaapi/testgenerator/EvoSuiteRunnerTest.kt
@@ -1,6 +1,7 @@
 package org.cafejojo.schaapi.testgenerator
 
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
@@ -26,15 +27,43 @@ internal class EvoSuiteRunnerTest : Spek({
 
     describe("execution of the EvoSuite test generator") {
         it("generates tests for a test class") {
-            EvoSuiteRunner(
+            val evoSuiteRunner = EvoSuiteRunner(
                     "org.cafejojo.schaapi.test.EvoSuiteTestClass",
                     classPath,
                     classPath,
                     generationTimeoutSeconds = 5
-            ).run()
+            )
+
+            evoSuiteRunner.run()
 
             assertThat(File("${evoSuiteTestOutput.path}/org/cafejojo/schaapi/test/EvoSuiteTestClass_ESTest.java"))
                     .exists()
+        }
+
+        it("throws an exception when the class can't be found on the given class path") {
+            val evoSuiteRunner = EvoSuiteRunner(
+                    "org.cafejojo.schaapi.test.EvoSuiteTestClass",
+                    ".",
+                    classPath,
+                    generationTimeoutSeconds = 5
+            )
+
+            assertThatThrownBy {
+                evoSuiteRunner.run()
+            }.isInstanceOf(EvoSuiteRuntimeException::class.java)
+        }
+
+        it("throws an exception when the class doesn't exist") {
+            val evoSuiteRunner = EvoSuiteRunner(
+                    "no.way.this.exists.SampleClass",
+                    classPath,
+                    classPath,
+                    generationTimeoutSeconds = 5
+            )
+
+            assertThatThrownBy {
+                evoSuiteRunner.run()
+            }.isInstanceOf(EvoSuiteRuntimeException::class.java)
         }
     }
 })
