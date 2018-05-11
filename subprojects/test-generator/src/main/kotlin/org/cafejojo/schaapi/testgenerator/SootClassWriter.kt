b65f52cd8c521078223704c9a3c431df6a0eb7a2
Georgios Andreadis <g.andreadis@student.tudelft.nl>
2018-05-12 00:44:08 +0200

    Add bytecode content test, restructure to object

--- subprojects/test-generator/src/main/kotlin/org/cafejojo/schaapi/testgenerator/SootClassWriter.kt
+++ subprojects/test-generator/src/main/kotlin/org/cafejojo/schaapi/testgenerator/SootClassWriter.kt
@@ -4,34 +4,47 @@ import soot.SootClass
 import soot.jimple.JasminClass
 import soot.util.JasminOutputStream
 import java.io.FileOutputStream
+import java.io.OutputStream
 import java.io.OutputStreamWriter
 import java.io.PrintWriter
 import java.nio.file.Paths
 
 /**
- * Writes a [SootClass] to a JVM bytecode class file.
- *
- * @property targetDirectory the path to the base directory in which to place the class file structure
+ * Functionality for serializing a [SootClass] to JVM bytecode.
  */
-class SootClassWriter(private val targetDirectory: String) {
+object SootClassWriter {
     /**
      * Writes the given [sootClass] to a class file.
      *
      * Creates the necessary package structure (based on the fully qualified name of the class) on the file system.
      * Creates all non-existing directories on the generated path.
      *
-     * @param sootClass the class to write to a file
+     * @param sootClass the class to write to file
+     * @param targetDirectory the path to the base directory in which to place the class file structure
      */
-    fun writeFile(sootClass: SootClass) {
+    fun writeToFile(sootClass: SootClass, targetDirectory: String) {
         val outputFile = Paths.get(targetDirectory, generateClassFilePath(sootClass.name)).toFile()
         outputFile.parentFile.mkdirs()
-        val outputStream = JasminOutputStream(FileOutputStream(outputFile))
-        val outputWriter = PrintWriter(OutputStreamWriter(outputStream))
+        val outputStream = FileOutputStream(outputFile)
+
+        writeToOutputStream(sootClass, outputStream)
+
+        outputStream.close()
+    }
+
+    /**
+     *  Writes the given [sootClass] to the given [outputStream].
+     *
+     * @param sootClass the class to write to a file
+     * @param outputStream the [OutputStream] to write the bytecode to
+     */
+    fun writeToOutputStream(sootClass: SootClass, outputStream: OutputStream) {
+        val jasminOutputStream = JasminOutputStream(outputStream)
+        val outputWriter = PrintWriter(OutputStreamWriter(jasminOutputStream))
 
         JasminClass(sootClass).print(outputWriter)
 
         outputWriter.flush()
-        outputStream.close()
     }
 
     private fun generateClassFilePath(fullyQualifiedName: String) = "${fullyQualifiedName.replace(".", "/")}.class"
--- subprojects/test-generator/src/test/kotlin/org/cafejojo/schaapi/testgenerator/SootClassWriterTest.kt
+++ subprojects/test-generator/src/test/kotlin/org/cafejojo/schaapi/testgenerator/SootClassWriterTest.kt
@@ -1,11 +1,14 @@
 package org.cafejojo.schaapi.testgenerator
 
+import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream
 import org.assertj.core.api.Assertions.assertThat
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
 import soot.Scene
 import java.nio.file.Paths
+import java.util.*
+import javax.xml.bind.DatatypeConverter
 
 internal class SootClassWriterTest : Spek({
     val classPath = Paths.get(SootClassWriterTest::class.java.getResource("../../../../").toURI()).toString()
@@ -29,8 +32,7 @@ internal class SootClassWriterTest : Spek({
         it("outputs a class file in the root directory") {
             val testClassName = "MyTestClass"
 
-            val sootClassWriter = SootClassWriter(classOutputDirectory.path)
-            sootClassWriter.writeFile(Scene.v().makeSootClass(testClassName))
+            SootClassWriter.writeToFile(Scene.v().makeSootClass(testClassName), classOutputDirectory.path)
 
             assertThat(Paths.get(classOutputDirectory.absolutePath, "$testClassName.class")).exists()
         }
@@ -39,9 +41,8 @@ internal class SootClassWriterTest : Spek({
             val testClassName1 = "MyFirstTestClass"
             val testClassName2 = "MySecondTestClass"
 
-            val sootClassWriter = SootClassWriter(classOutputDirectory.path)
-            sootClassWriter.writeFile(Scene.v().makeSootClass(testClassName1))
-            sootClassWriter.writeFile(Scene.v().makeSootClass(testClassName2))
+            SootClassWriter.writeToFile(Scene.v().makeSootClass(testClassName1), classOutputDirectory.path)
+            SootClassWriter.writeToFile(Scene.v().makeSootClass(testClassName2), classOutputDirectory.path)
 
             assertThat(Paths.get(classOutputDirectory.absolutePath, "$testClassName1.class")).exists()
             assertThat(Paths.get(classOutputDirectory.absolutePath, "$testClassName2.class")).exists()
@@ -50,10 +51,21 @@ internal class SootClassWriterTest : Spek({
         it("outputs a class file in a directory structure") {
             val testClassName = "org.test.MyTestClass"
 
-            val sootClassWriter = SootClassWriter(classOutputDirectory.path)
-            sootClassWriter.writeFile(Scene.v().makeSootClass(testClassName))
+            SootClassWriter.writeToFile(Scene.v().makeSootClass(testClassName), classOutputDirectory.path)
 
             assertThat(Paths.get(classOutputDirectory.absolutePath, "org", "test", "MyTestClass.class")).exists()
+        }
+
+        it("outputs the same bytecode for a simple class, consistently") {
+            val testClassName = "MyTestClass"
+
+            val byteOutputStream = ByteOutputStream()
+            SootClassWriter.writeToOutputStream(Scene.v().makeSootClass(testClassName), byteOutputStream)
+
+            assertThat(DatatypeConverter.printBase64Binary(byteOutputStream.bytes))
+                    .isEqualTo("yv66vgAAAC4AAwcAAgEAC015VGVzdENsYXNzACAAAQ" +
+                            Collections.nCopies(1324, "A").joinToString(separator = "") { it } +
+                            "==")
         }
     }
 })
