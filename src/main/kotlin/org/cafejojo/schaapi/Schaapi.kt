cae4008dcc728419cc5fc2b00835773f89f7a935
Felix Dekker <felix@fwdekker.com>
2018-05-12 10:41:13 +0200

    Implement preliminary pipeline

--- src/main/kotlin/org/cafejojo/schaapi/Schaapi.kt
+++ src/main/kotlin/org/cafejojo/schaapi/Schaapi.kt
@@ -1,8 +1,35 @@
 package org.cafejojo.schaapi
 
+import org.cafejojo.schaapi.patterndetector.PathEnumerator
+import org.cafejojo.schaapi.patterndetector.PatternDetector
+import org.cafejojo.schaapi.projectcompiler.JavaMavenProject
+import org.cafejojo.schaapi.usagegraphgenerator.SootProjectLibraryUsageGraphGenerator
+import java.io.File
+
+private const val PATTERN_DETECTOR_MINIMUM = 3
+
 /**
- * Placeholder main method.
+ * Runs the complete first phase of the Schaapi pipeline.
+ *
+ * @param args the path to the library project and the paths to the user projects
  */
 fun main(args: Array<String>) {
-    println("I am the main main class!")
+    if (args.size < 2) {
+        println("Expected at least 2 arguments but received ${args.size}.")
+        return
+    }
+
+    val library = JavaMavenProject(File(args[0]))
+    val users = args.takeLast(args.size - 1).map { JavaMavenProject(File(it)) }
+
+    library.compile()
+    users.forEach { it.compile() }
+
+    val graphGenerator = SootProjectLibraryUsageGraphGenerator
+    val userGraphs = users.map { graphGenerator.generate(library, it) }
+    val userPaths = userGraphs.flatMap { it.flatMap { it.flatMap { PathEnumerator(it).enumerate() } } }
+
+    /*val patterns = */PatternDetector(userPaths, PATTERN_DETECTOR_MINIMUM).findFrequentSequences()
+
+    // Generate testable class here
 }
