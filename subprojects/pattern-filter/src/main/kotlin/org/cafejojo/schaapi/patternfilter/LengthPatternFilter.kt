386a27189ef513232a551871b8c34f322f7be4a6
Joel Abrahams <abrahamsjo@gmail.com>
2018-05-16 13:13:52 +0200

    Add TooSmallMinimumPatternLengthException
    * Improve documentation slightly
    * Add extra test case for exception

--- subprojects/pattern-filter/src/main/kotlin/org/cafejojo/schaapi/patternfilter/LengthPatternFilter.kt
+++ subprojects/pattern-filter/src/main/kotlin/org/cafejojo/schaapi/patternfilter/LengthPatternFilter.kt
@@ -8,9 +8,19 @@ private const val DEFAULT_MINIMUM_PATTERN_LENGTH = 2
 /**
  * Filters out patterns that are too short.
  *
- * @property minimumLength the minimum length a pattern should have for it to be retained.
+ * @property minimumLength the minimum length (inclusive) a pattern should have for it to be retained.
  * [DEFAULT_MINIMUM_PATTERN_LENGTH] by default
  */
 class LengthPatternFilter(private val minimumLength: Int = DEFAULT_MINIMUM_PATTERN_LENGTH) : PatternFilter {
+    init {
+        if (minimumLength < 1) throw TooSmallMinimumPatternLengthException(minimumLength)
+    }
+
     override fun retain(pattern: List<Node>): Boolean = pattern.size >= minimumLength
 }
+
+/**
+ * A [RuntimeException] occurring when the passed minimum length is less than 1.
+ */
+class TooSmallMinimumPatternLengthException(length: Int) :
+    RuntimeException("A minimum pattern length must be 1 or greater, was $length.")
--- subprojects/pattern-filter/src/test/kotlin/org/cafejojo/schaapi/patternfilter/LengthPatternFilterTest.kt
+++ subprojects/pattern-filter/src/test/kotlin/org/cafejojo/schaapi/patternfilter/LengthPatternFilterTest.kt
@@ -1,22 +1,32 @@
 package org.cafejojo.schaapi.patternfilter
 
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
 
 class LengthPatternFilterTest : Spek({
     describe("when filtering a pattern") {
-        it("should give true for a pattern equal to the default length") {
+        it("should return true for a pattern equal to the default length") {
             assertThat(LengthPatternFilter().retain(listOf(TestNode(), TestNode()))).isTrue()
         }
 
-        it("should give false for a pattern shorter than the default length") {
+        it("should return false for a pattern shorter than the default length") {
             assertThat(LengthPatternFilter().retain(listOf(TestNode()))).isFalse()
         }
 
-        it("should give true for a pattern longer than the passed length") {
+        it("should return false for a pattern shorter than the default length") {
+            assertThat(LengthPatternFilter().retain(listOf(TestNode()))).isFalse()
+        }
+
+        it("should return true for a pattern longer than the passed length") {
             assertThat(LengthPatternFilter(1).retain(listOf(TestNode(), TestNode()))).isTrue()
+        }
+
+        it("should give an exception if the given pattern length is 0") {
+            assertThatThrownBy { LengthPatternFilter(0) }
+                .isInstanceOf(TooSmallMinimumPatternLengthException::class.java)
         }
     }
 })
