94436d01da02429199ca3c7de6e676871275a5ad
Casper Boone <mail@casperboone.nl>
2018-05-20 14:40:52 +0200

    Improve PatternFilter readability and add test

--- modules/common/src/main/kotlin/org/cafejojo/schaapi/common/PatternFilter.kt
+++ modules/common/src/main/kotlin/org/cafejojo/schaapi/common/PatternFilter.kt
@@ -5,12 +5,12 @@ package org.cafejojo.schaapi.common
  *
  * @property rules rules that indicated whether a pattern should be retained or not
  */
-class PatternFilter(private vararg val rules: PatternFilterRule ) {
+class PatternFilter(private vararg val rules: PatternFilterRule) {
     /**
      * Performs filtering of patterns based on the gives list of filtering rules.
      *
      * @param patterns list of patterns to be filtered
      * @return list of filtered patterns
      */
-    fun filter(patterns: List<Pattern>) = patterns.filter { pattern -> rules.map { it.retain(pattern) }.all { it } }
+    fun filter(patterns: List<Pattern>) = patterns.filter { pattern -> rules.all { rule -> rule.retain(pattern) } }
 }
