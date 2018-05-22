7e406ab3dad6b31b08034acb65cda0038ccb3bc1
Felix Dekker <felix@fwdekker.com>
2018-05-22 16:13:37 +0200

    Add simple RecursiveValue implementation of Value

--- modules/models/jimple-library-usage-graph/src/test/kotlin/org/cafejojo/schaapi/models/libraryusagegraph/jimple/SimpleValue.kt
+++ modules/models/jimple-library-usage-graph/src/test/kotlin/org/cafejojo/schaapi/models/libraryusagegraph/jimple/SimpleValue.kt
@@ -8,31 +8,40 @@ import soot.ValueBox
 import soot.util.Switch
 
 /**
- * A simple implementation of [Value] that implements only the functionality for comparing [Value]s.
+ * A [Value] that does not contain other values, and that implements only functionality related to equivalence.
  */
-class SimpleValue(private val type: String) : Value {
-    override fun equivTo(other: Any?) = other is SimpleValue && this.type == other.type
+class SimpleValue(type: String) : RecursiveValue(type)
+
+/**
+ * A [Value] that may contain other values, and that implements only functionality related to equivalence.
+ */
+open class RecursiveValue(private val type: String, private vararg val subValues: Value) : Value {
+    override fun equivTo(other: Any?) =
+        other is RecursiveValue
+            && this.type == other.type
+            && this.subValues.size == other.subValues.size
+            && this.subValues.zip(other.subValues).all { it.first.equivTo(it.second) }
 
     override fun getType(): Type = RefType.v(type)
 
     override fun apply(switch: Switch?) {
-        throw NotImplementedError("`apply` is not implemented in `SimpleValue`.")
+        throw NotImplementedError("`apply` is not implemented in `RecursiveValue`.")
     }
 
     override fun clone(): Any {
-        throw NotImplementedError("`clone` is not implemented in `SimpleValue`.")
+        throw NotImplementedError("`clone` is not implemented in `RecursiveValue`.")
     }
 
     @SuppressWarnings("ExceptionRaisedInUnexpectedLocation")
     override fun toString(up: UnitPrinter?) {
-        throw NotImplementedError("`toString` is not implemented in `SimpleValue`.")
+        throw NotImplementedError("`toString` is not implemented in `RecursiveValue`.")
     }
 
     override fun equivHashCode(): Int {
-        throw NotImplementedError("`equivHashCode` is not implemented in `SimpleValue`.")
+        throw NotImplementedError("`equivHashCode` is not implemented in `RecursiveValue`.")
     }
 
     override fun getUseBoxes(): MutableList<ValueBox> {
-        throw NotImplementedError("`getUseBoxes` is not implemented in `SimpleValue`.")
+        throw NotImplementedError("`getUseBoxes` is not implemented in `RecursiveValue`.")
     }
 }
