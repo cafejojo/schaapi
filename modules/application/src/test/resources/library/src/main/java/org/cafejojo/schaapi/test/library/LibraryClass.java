e06d4083577e2d56144d1086547ea9d62cb684c9
Georgios Andreadis <g.andreadis@student.tudelft.nl>
2018-05-23 13:17:45 +0200

    Adapt smoke test to include multiple projects

--- modules/application/src/test/resources/library/src/main/java/org/cafejojo/schaapi/test/library/LibraryClass.java
+++ modules/application/src/test/resources/library/src/main/java/org/cafejojo/schaapi/test/library/LibraryClass.java
@@ -1,7 +1,13 @@
 package org.cafejojo.schaapi.test.library;
 
 public final class LibraryClass {
+    public int foo = 231;
+
     public int libraryFoo() {
-        return 15;
+        return 841;
+    }
+
+    public int libraryBar() {
+        return 810;
     }
 }
