9a765a98d3eb38b5de6820a102de4e4b089123ca
Georgios Andreadis <g.andreadis@student.tudelft.nl>
2018-05-24 14:10:45 +0200

    Fix handling of method with only one throw stmt
    At the moment, the implementation fails on methods with only one throw-statement in them, if the exception class is not part of the library. Fixes this and also adds a test case for it.
    
    Co-Authored-By: Casper Boone <casperboone@users.noreply.github.com>

---
+++ modules/pipeline/jimple-library-usage-graph-generator/src/test/java/org/cafejojo/schaapi/pipeline/usagegraphgenerator/jimple/testclasses/users/ThrowOtherExceptionTest.java
@@ -0,0 +1,7 @@
+package org.cafejojo.schaapi.pipeline.usagegraphgenerator.jimple.testclasses.users;
+
+public class ThrowOtherExceptionTest {
+    public void foo() {
+        throw new RuntimeException();
+    }
+}
--- modules/pipeline/jimple-library-usage-graph-generator/src/test/kotlin/org/cafejojo/schaapi/pipeline/usagegraphgenerator/jimple/IntegrationTest.kt
+++ modules/pipeline/jimple-library-usage-graph-generator/src/test/kotlin/org/cafejojo/schaapi/pipeline/usagegraphgenerator/jimple/IntegrationTest.kt
@@ -294,6 +294,18 @@ internal class IntegrationTest : Spek({
             )
         }
 
+        it("converts a class containing a throw statement without library usage to a filtered cfg") {
+            val cfg = LibraryUsageGraphGenerator.generate(
+                libraryProject,
+                TestProject(testClassesClassPath, listOf("$TEST_CLASSES_PACKAGE.users.ThrowOtherExceptionTest"))
+            )[1]
+
+            assertThatStructureMatches(
+                node<JReturnVoidStmt>(),
+                cfg
+            )
+        }
+
         it("converts a class containing a try-catch statement with library usage in the try block to a filtered cfg") {
             val cfg = LibraryUsageGraphGenerator.generate(
                 libraryProject,
