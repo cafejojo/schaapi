239cfcccf6ed84eec1cc1799ea671ab289d9ae74
Joel Abrahams <abrahamsjo@gmail.com>
2018-05-30 11:17:57 +0200

    Use proper order of generics in GitHubSearchOptions

--- modules/pipeline/github-project-miner/src/main/kotlin/org/cafejojo/schaapi/pipeline/miner/github/ProjectMiner.kt
+++ modules/pipeline/github-project-miner/src/main/kotlin/org/cafejojo/schaapi/pipeline/miner/github/ProjectMiner.kt
@@ -24,7 +24,7 @@ class ProjectMiner<P : Project>(
     private val username: String, private val password: String,
     private val outputDirectory: File,
     private val projectPacker: (File) -> P
-) : ProjectMiner<P, GitHubSearchOptions<P>> {
+) : ProjectMiner<GitHubSearchOptions<P>, P> {
     init {
         if (!outputDirectory.isDirectory) outputDirectory.mkdirs()
     }
