0fdc2d0f156d0295d76003f03b622c2cfcfc50d4
Casper Boone <mail@casperboone.nl>
2018-06-04 17:51:35 +0200

    Switch JSON builder implementation to a 'DSL'

---
+++ modules/github-test-reporter/src/main/kotlin/org/cafejojo/schaapi/githubtestreporter/JsonBuilder.kt
@@ -0,0 +1,23 @@
+package org.cafejojo.schaapi.githubtestreporter
+
+import java.util.ArrayDeque
+import java.util.Deque
+import org.json.JSONObject
+
+internal fun json(build: JsonObjectBuilder.() -> Unit): JSONObject {
+    return JsonObjectBuilder().json(build)
+}
+
+internal class JsonObjectBuilder {
+    private val deque: Deque<JSONObject> = ArrayDeque()
+
+    fun json(build: JsonObjectBuilder.() -> Unit): JSONObject {
+        deque.push(JSONObject())
+        this.build()
+        return deque.pop()
+    }
+
+    infix fun <T> String.to(value: T) {
+        deque.peek().put(this, value)
+    }
+}
