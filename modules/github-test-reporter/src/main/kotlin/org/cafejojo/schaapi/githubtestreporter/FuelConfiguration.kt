471d8487dcb0bb3b42221833f1b2e3da14963b7c
Casper Boone <mail@casperboone.nl>
2018-06-05 12:11:29 +0200

    Allow all supported HTTP clients

--- modules/github-test-reporter/src/main/kotlin/org/cafejojo/schaapi/githubtestreporter/FuelConfiguration.kt
+++ modules/github-test-reporter/src/main/kotlin/org/cafejojo/schaapi/githubtestreporter/FuelConfiguration.kt
@@ -1,11 +1,12 @@
 package org.cafejojo.schaapi.githubtestreporter
 
+import com.github.kittinunf.fuel.core.Client
 import com.github.kittinunf.fuel.core.FuelManager
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-internal open class FuelConfiguration(customHttpClient: CustomHttpClient) {
+internal open class FuelConfiguration(httpClient: Client) {
     init {
-        FuelManager.instance.client = customHttpClient
+        FuelManager.instance.client = httpClient
     }
 }
