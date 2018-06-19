4da2825789db81b690baf21fe01c97359b492d72
Casper Boone <mail@casperboone.nl>
2018-06-19 12:06:34 +0200

    Compile library and tests, and run tests

--- modules/validation-pipeline/ci-job/src/main/kotlin/org/cafejojo/schaapi/validationpipeline/cijob/CIJob.kt
+++ modules/validation-pipeline/ci-job/src/main/kotlin/org/cafejojo/schaapi/validationpipeline/cijob/CIJob.kt
@@ -3,22 +3,41 @@ package org.cafejojo.schaapi.validationpipeline.cijob
 import com.github.kittinunf.fuel.Fuel
 import com.github.kittinunf.fuel.core.Request
 import com.github.kittinunf.result.Result
+import mu.KLogging
+import org.cafejojo.schaapi.miningpipeline.projectcompiler.javamaven.ProjectCompiler
+import org.cafejojo.schaapi.models.project.JavaMavenProject
+import org.cafejojo.schaapi.validationpipeline.TestResults
+import org.cafejojo.schaapi.validationpipeline.testrunner.junit.TestRunner
 import org.zeroturnaround.zip.ZipUtil
+import java.io.ByteArrayOutputStream
 import java.io.File
+import javax.tools.ToolProvider
 
 /**
  * A CI job that performs the execution of all involved steps.
  */
-class CIJob(private val identifier: String, private val projectDirectory: File, private val downloadUrl: String) {
+class CIJob(
+    private val identifier: String,
+    private val projectDirectory: File,
+    private val downloadUrl: String
+) {
     private val zipFile = File(projectDirectory, "builds").let { it.mkdirs(); File(it, "$identifier.zip") }
     private val newProjectFiles = File(projectDirectory, "builds/$identifier").also { it.mkdirs() }
+    private val testsDirectory = File(projectDirectory, "tests")
+
+    private companion object : KLogging()
 
     /**
      * Executes the necessary steps to run tests and report the results.
      */
-    fun run() {
-        download()
-        extractZip()
+    fun run(): TestResults {
+        if (!testsDirectory.exists()) throw CIJobException("This project has not been initialized yet. $testsDirectory")
+
+        next("Start downloading...") { download() }
+        next("Extract zip...") { extractZip() }
+        val library = next("Start compile library...") { compileLibrary() }
+        val tests = next( "Start compile tests...") { compileTests(library) }
+        return next("Start running tests...") { TestRunner().run(testsDirectory, tests, listOf(library.classDir)) }
     }
 
     private fun download() = Fuel.head(downloadUrl).responseOrThrowException().let { (_, urlResponse, _) ->
@@ -27,7 +46,36 @@ class CIJob(private val identifier: String, private val projectDirectory: File, 
             .responseOrThrowException()
     }
 
-    private fun extractZip() = ZipUtil.unpack(zipFile, newProjectFiles) { it.substringAfter("/") }
+    private fun extractZip() = ZipUtil.unpack(zipFile, newProjectFiles, { it.substringAfter("/") })
+
+    private fun compileLibrary() = ProjectCompiler().compile(JavaMavenProject(newProjectFiles))
+
+    private fun compileTests(library: JavaMavenProject): List<File> {
+        val compiler = ToolProvider.getSystemJavaCompiler()
+        val errorOutput = ByteArrayOutputStream()
+
+        val classPath = mutableListOf<String>().run {
+            addAll(testsDirectory.listFiles().filter { it.extension == "class" }.map { it.parent })
+            add(library.classDir.absolutePath)
+            add(CIJob::class.java.getResource("/junit-4.12.jar").file)
+
+            joinToString(File.pathSeparator)
+        }
+
+        return testsDirectory.listFiles().filter { it.extension == "java" }
+            .also { files ->
+                if (files.any { compiler.run(null, null, errorOutput, it.absolutePath, "-cp", classPath) != 0 }) {
+                    throw CIJobException("Compilation failed:\n\n${errorOutput.toString("UTF-8")}")
+                }
+            }
+            .map { File(testsDirectory, "${it.nameWithoutExtension}.class") }
+    }
+
+    private fun <T> next(logMessage: String, action: () -> T): T {
+        logger.info("[build-$projectDirectory-$identifier] $logMessage")
+
+        return action()
+    }
 }
 
 internal fun Request.responseOrThrowException() = response().also { (_, _, result) ->
@@ -36,3 +84,8 @@ internal fun Request.responseOrThrowException() = response().also { (_, _, resul
         is Result.Failure -> throw result.getException()
     }
 }
+
+/**
+ * Exception that gets thrown when an error occurs during the execution of a CI job.
+ */
+class CIJobException(message: String) : Exception(message)
--- modules/validation-pipeline/ci-job/src/test/kotlin/org/cafejojo/schaapi/validationpipeline/cijob/CIJobTest.kt
+++ modules/validation-pipeline/ci-job/src/test/kotlin/org/cafejojo/schaapi/validationpipeline/cijob/CIJobTest.kt
@@ -7,18 +7,30 @@ import java.io.File
 import java.nio.file.Files
 
 object CIJobTest : Spek({
+    lateinit var storageDirectory: File
     lateinit var projectDirectory: File
 
     beforeEachTest {
-        projectDirectory = Files.createTempDirectory("schaapi-ci").toFile()
+        storageDirectory = Files.createTempDirectory("schaapi-ci").toFile()
+        projectDirectory = File(storageDirectory, "projects/cafejojo/dummy-simple-maven-library").also { it.mkdirs() }
+
+        val testsDirectory = File(projectDirectory, "tests").also { it.mkdir() }
+
+        File(CIJobTest::class.java.getResource("/projects/cafejojo/dummy-simple-maven-library/tests").file)
+            .listFiles()
+            .forEach { it.copyTo(File(testsDirectory, it.name), overwrite = true) }
     }
 
     it("executes a CI job") {
-        val commitHash = "4f392a58006d64c4ac3259bee14b0bf4484a431f"
-        val ciJob = CIJob(commitHash, projectDirectory, "https://github.com/cafejojo/schaapi/archive/$commitHash.zip")
+        val commitHash = "ced7a679ba6337977d99effccdb4ac66b3ba34e0"
+        val downloadUrl = "https://github.com/cafejojo/dummy-simple-maven-library/archive/$commitHash.zip"
 
-        ciJob.run()
+        val ciJob = CIJob(commitHash, projectDirectory, downloadUrl)
 
-        assertThat(File(projectDirectory, "builds/$commitHash/LICENSE")).exists()
+        val testResults = ciJob.run()
+
+        assertThat(File(projectDirectory, "builds/$commitHash/README.md")).exists()
+        assertThat(testResults.totalCount).isEqualTo(1)
+        assertThat(testResults.passCount).isEqualTo(1)
     }
 })
