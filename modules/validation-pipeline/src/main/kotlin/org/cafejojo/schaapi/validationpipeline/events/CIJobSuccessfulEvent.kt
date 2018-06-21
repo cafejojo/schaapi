2a6c75aaf37feff0cbdb9cc84bcc7275f97093fb
Casper Boone <mail@casperboone.nl>
2018-06-21 11:53:00 +0200

    Remove old event

--- modules/validation-pipeline/src/main/kotlin/org/cafejojo/schaapi/validationpipeline/events/CIJobSuccessfulEvent.kt
+++
@@ -1,8 +0,0 @@
-package org.cafejojo.schaapi.validationpipeline.events
-
-import org.cafejojo.schaapi.validationpipeline.TestResults
-
-/**
- * Event that can be broadcast after a CI job run is completed successfully.
- */
-data class CIJobSuccessfulEvent(val testResults: TestResults)
