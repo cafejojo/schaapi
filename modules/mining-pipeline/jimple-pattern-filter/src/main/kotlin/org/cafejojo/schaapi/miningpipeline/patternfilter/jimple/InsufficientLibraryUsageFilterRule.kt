828982da61dcb9870b4f4f1b4c00a832b832fbdb
Felix Dekker <felix@fwdekker.com>
2018-06-28 14:50:07 +0200

    Fix compilation issues

--- modules/mining-pipeline/jimple-pattern-filter/src/main/kotlin/org/cafejojo/schaapi/miningpipeline/patternfilter/jimple/InsufficientLibraryUsageFilterRule.kt
+++ modules/mining-pipeline/jimple-pattern-filter/src/main/kotlin/org/cafejojo/schaapi/miningpipeline/patternfilter/jimple/InsufficientLibraryUsageFilterRule.kt
@@ -5,7 +5,6 @@ import org.cafejojo.schaapi.miningpipeline.PatternFilterRule
 import org.cafejojo.schaapi.models.libraryusagegraph.jimple.JimpleNode
 import org.cafejojo.schaapi.models.libraryusagegraph.jimple.JimpleValueVisitor
 import org.cafejojo.schaapi.models.project.JavaProject
-import soot.Immediate
 import soot.Value
 
 /**
@@ -40,23 +39,7 @@ class LibraryUsageVisitor(private val libraryProject: JavaProject) : JimpleValue
      * @param value a [Value]
      * @return false
      */
-    override fun defaultApply(value: Value) = false
-
-    /**
-     * Returns true iff the type of [value] is a library class.
-     *
-     * @param value an [Immediate]
-     * @return true iff the type of [value] is a library class
-     */
-    override fun apply(value: Immediate) = isLibraryClass(value.type.toString())
-
-    /**
-     * Returns true iff [className] is a class defined in [libraryProject].
-     *
-     * @param className a class name
-     * @return true iff [className] is a class defined in [libraryProject]
-     */
-    private fun isLibraryClass(className: String) = libraryProject.classNames.contains(className)
+    override fun applyDefault(value: Value) = isLibraryClass(value.type.toString())
 
     /**
      * Returns true iff [result1] and [result2] are both true.
@@ -66,4 +49,12 @@ class LibraryUsageVisitor(private val libraryProject: JavaProject) : JimpleValue
      * @return true iff [result1] and [result2] are both true
      */
     override fun accumulate(result1: Boolean, result2: Boolean) = result1 && result2
+
+    /**
+     * Returns true iff [className] is a class defined in [libraryProject].
+     *
+     * @param className a class name
+     * @return true iff [className] is a class defined in [libraryProject]
+     */
+    private fun isLibraryClass(className: String) = libraryProject.classNames.contains(className)
 }
