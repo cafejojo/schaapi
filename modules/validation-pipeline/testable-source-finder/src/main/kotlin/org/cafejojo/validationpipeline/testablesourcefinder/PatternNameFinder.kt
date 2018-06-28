c0efc92b2653eb870fce2ec3b810cc420df9f452
Casper Boone <mail@casperboone.nl>
2018-06-28 10:13:35 +0200

    Add PatternNameFinder

---
+++ modules/validation-pipeline/testable-source-finder/src/main/kotlin/org/cafejojo/validationpipeline/testablesourcefinder/PatternNameFinder.kt
@@ -0,0 +1,17 @@
+package org.cafejojo.validationpipeline.testablesourcefinder
+
+import java.io.File
+
+/**
+ * Finds pattern names within a test file.
+ */
+class PatternNameFinder(private val searchableSource: String) {
+    private val query = Regex(".*Patterns\\.(pattern\\d+)\\(.*")
+
+    /**
+     * Finds pattern names within a test file.
+     */
+    fun find() = searchableSource.lines().mapNotNull {
+        query.find(it)?.destructured?.component1()
+    }
+}
