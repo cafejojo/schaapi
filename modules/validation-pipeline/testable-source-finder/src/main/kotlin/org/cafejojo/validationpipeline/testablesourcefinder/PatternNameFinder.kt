79c0dcb11931e5b554e94e83d81ca0f1478507b8
Casper Boone <mail@casperboone.nl>
2018-06-29 13:56:25 +0200

    Clarify PatternNameFinder kdoc

--- modules/validation-pipeline/testable-source-finder/src/main/kotlin/org/cafejojo/validationpipeline/testablesourcefinder/PatternNameFinder.kt
+++ modules/validation-pipeline/testable-source-finder/src/main/kotlin/org/cafejojo/validationpipeline/testablesourcefinder/PatternNameFinder.kt
@@ -1,13 +1,13 @@
 package org.cafejojo.validationpipeline.testablesourcefinder
 
 /**
- * Finds pattern names within a test file.
+ * Responsible for finding pattern names within a test file.
  */
 class PatternNameFinder(private val searchableSource: String) {
     private val query = Regex(".*Patterns\\.(pattern\\d+)\\(.*")
 
     /**
-     * Finds pattern names within a test file.
+     * Finds all pattern names within a test file.
      */
     fun find() = searchableSource.lines().mapNotNull {
         query.find(it)?.destructured?.component1()
